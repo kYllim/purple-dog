7890edb33e1b80ab2c7761cc8bf9ab91745063b4
VM Dane <vm_dane@MacBook-Air-de-VMD.local>
2025-12-08 16:16:23 +0100

    Initial Frontend Vue + Tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        text: "#0F172A",
+        background: "#F1F5F9",
+        accent: "#C5A059",
+      },
+    },
+  },
+  plugins: [],
+}
