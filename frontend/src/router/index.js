d150adf5c1bca0c6582be3be66d4bd011d15392c
Arthur serenne <arthur.serenne@gmail.com>
2025-12-09 10:54:13 +0100

    ajout du dashboard admin

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,12 +4,22 @@ import Home from '../pages/Home.vue';
 import Login from '../pages/Login.vue';
 import Register from '../pages/Register.vue';
 import Error404 from '../pages/Error404.vue';
+import AdminLayout from '../layouts/AdminLayout.vue';
+import AdminDashboard from '../pages/admin/Dashboard.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
 
   { path: '/login', name: 'Login', component: Login },
   { path: '/register', name: 'Register', component: Register },
+
+  {
+    path: '/admin',
+    component: AdminLayout,
+    children: [
+      { path: '', name: 'AdminDashboard', component: AdminDashboard }
+    ]
+  },
 
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: Error404 }
 ];
