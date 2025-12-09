abcc000aa874be44432267da13451047d8fa3829
Arthur serenne <arthur.serenne@gmail.com>
2025-12-09 14:51:05 +0100

    modification du rooter

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,17 +3,17 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../pages/Home.vue';
 import Login from '../pages/Login.vue';
 import Register from '../pages/Register.vue';
+import ProfileEdit from '../pages/ProfileEdit.vue';
 import Error404 from '../pages/Error404.vue';
 import AdminLayout from '../layouts/AdminLayout.vue';
 import AdminDashboard from '../pages/admin/Dashboard.vue';
-
-
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
 
   { path: '/login', name: 'Login', component: Login },
   { path: '/register', name: 'Register', component: Register },
+  { path: '/profile/edit', name: 'ProfileEdit', component: ProfileEdit },
 
   {
     path: '/admin',
