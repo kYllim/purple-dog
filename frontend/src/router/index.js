f1b94945405745f4166c60266b17118054195ab6
kYllim <130597619+kYllim@users.noreply.github.com>
2025-12-09 22:04:29 +0100

    cards historique et favoris

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,12 +4,17 @@ import Home from '../pages/Home.vue';
 import Login from '../pages/Login.vue';
 import Register from '../pages/Register.vue';
 import Error404 from '../pages/Error404.vue';
+import Favorite from '../components/cards/FavoriteCard.vue';
+import History from '../components/cards/HistoryCard.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
 
   { path: '/login', name: 'Login', component: Login },
   { path: '/register', name: 'Register', component: Register },
+  { path: '/favorites', name: 'Favorites', component: Favorite },
+  { path: '/history', name: 'History', component: History },
+
 
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: Error404 }
 ];
